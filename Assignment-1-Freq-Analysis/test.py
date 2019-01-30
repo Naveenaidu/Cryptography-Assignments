0fec2a3f207ccb4571825da725fe37e0e7ca28ff
Naveenaidu <naveennaidu479@gmail.com>
2019-01-31 00:04:12 +0530

    Add Alphabet Count of Cipher Text

--- Assignment-1-Freq-Analysis/test.py
+++ Assignment-1-Freq-Analysis/test.py
@@ -11,7 +11,7 @@ def monolithic_subtitution(input_file,output_file):
 		input_file : The plaintext file 
 		output_file: The file with the monolithic substitution
 
-	alphabets: List of dictionary where {'key':'value'} refers to
+	alphabets: Dictionary where {'key':'value'} refers to
 	           {'letter':'substitution-letter'}
 	"""
 
@@ -23,7 +23,7 @@ def monolithic_subtitution(input_file,output_file):
 	             'z':'C'}
 
 	while True:
-		ch = input_file.read(1)
+		ch = input_file.read(1) #Reading file char by char
 		if not ch: 
 			input_file.close()
 			output_file.close()
@@ -35,13 +35,48 @@ def monolithic_subtitution(input_file,output_file):
 		except:
 			continue	
 
+def count_chars(cipher_file):
+	"""
+	Counts the occurences of each letter in the cipher text
+
+	param:
+		cipher_file: The file encrypted by monolithic substitution
+
+	alphabets_count: Dictionary where {'key':'value'} refers to
+	                 {'letter':'no-of-occurences'}	
+
+
+	"""
+
+	alphabets_count = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0,
+	                  'f':0, 'g':0, 'h':0, 'i':0, 'j':0,
+	                  'k':0, 'l':0, 'm':0, 'n':0, 'o':0,
+	                  'p':0, 'q':0, 'r':0, 's':0, 't':0,
+	                  'u':0, 'v':0, 'w':0, 'x':0, 'y':0,
+	                  'z':0}
+
+	while True:
+		ch = cipher_file.read(1) #Reading file char by char
+		if not ch:
+			cipher_file.close()
+			break
+		try:
+			ch = ch.lower()
+			alphabets_count[ch] = alphabets_count[ch] + 1
+		except:
+			continue
+
+	return(alphabets_count)		
+
 
 def main():
 	
 	input_file = open("plain.txt","r")
 	output_file = open("cipher.txt","w")
-
-	monolithic_subtitution(input_file,output_file)	             
+    monolithic_subtitution(input_file,output_file)
+	
+	cipher_file = open("cipher.txt","r")
+	alphabets_count = count_chars(cipher_file)	             
 	            
 if __name__ == '__main__':
 	main()
